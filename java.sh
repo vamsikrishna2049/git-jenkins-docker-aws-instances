51a8a01fe18091d7b6deec55fd87d6803efc159a
vamsi <118727057+vamsikrishna2049@users.noreply.github.com>
2023-06-30 20:18:58 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-sudo apt-get update -y
+#update the local repo
+sudo apt update -y
+#installing default jre
 sudo apt-get install default-jre -y
+#installing default jdk
 sudo apt-get install default-jdk -y
-
+#set the java path
 java_path=$(readlink -f /usr/bin/java)
+#printing the java home 
 echo "JAVA_HOME=$java_path" | sudo tee -a /etc/environment
-
 source /etc/environment
